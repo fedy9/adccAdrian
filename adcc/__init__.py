5a21374de545d6ab2efd3be14d54e6364651bca6
fedy9 <adrianmueller97@gmx.de>
2023-07-17 10:52:53 +0200

    updated authors

--- adcc/__init__.py
+++ adcc/__init__.py
@@ -70,7 +70,7 @@ __version__ = "0.15.14"
 __license__ = "GPL v3"
 __url__ = "https://adc-connect.org"
 __authors__ = ["Michael F. Herbst", "Maximilian Scheurer", "Jonas Leitner",
-               "Antonia Papapostolou"]
+               "Antonia Papapostolou", "Adrian Mueller"]
 __email__ = "developers@adc-connect.org"
 __contributors__ = []
 
